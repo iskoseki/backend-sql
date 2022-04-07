c142f6641fc9c87faba1cf27137c283742d8a542
Leandro Bordon <62522553+Alocerio@users.noreply.github.com>
2022-04-07 17:04:11 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'backend-side-sql'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'Mayorista21'),
+      database: env('DATABASE_NAME', 'strapi_bd'),
+      user: env('DATABASE_USERNAME', 'Strapiadmin'),
+      password: env('DATABASE_PASSWORD', 'Mayorista@2021'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
